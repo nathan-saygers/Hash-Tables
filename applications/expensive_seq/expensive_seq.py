d279724e979f4cec12bc0305bb3f005cf36ef0f5
nathan-saygers <nathan.saygers@gmail.com>
2020-05-07 13:29:44 -0700

    Passing fast test

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,17 +1,19 @@
+cache = {}
+
+
 def expensive_seq(x, y, z):
+
     if x <= 0:
         return y + z
+    elif cache.get((x, y, z)) != None:
+        return cache[(x, y, z)]
+    else:
+        result = expensive_seq(x - 1, y + 1, z) + \
+                expensive_seq(x - 2, y + 2, z*2) + \
+                expensive_seq(x - 3, y + 3, z*3)
 
-    cache = {}
-
-    result = expensive_seq(x - 1, y + 1, z) + \
-            expensive_seq(x - 2, y + 2, z*2) + \
-            expensive_seq(x - 3, y + 3, z*3)
-
-    if result not in cache:
         cache[(x, y, z)] = result
-
-    return cache[(x, y, z)]
+        return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
@@ -19,7 +21,7 @@ if __name__ == "__main__":
         x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
-    print(expensive_seq(150, 400, 800))
+    # print(expensive_seq(150, 400, 800))
 
 # Other FIB:
 
