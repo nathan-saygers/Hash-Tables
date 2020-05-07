b5d893ca241ed19712015d50b6675b57c12edc7c
nathan-saygers <nathan.saygers@gmail.com>
2020-05-07 14:42:11 -0700

    Lookup table runs in < 10 seconds

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -2,32 +2,31 @@ import math
 import random
 
 
+def build_lookup_table():
+    table = {}
+    for x in range(2, 14):
+        for y in range(3, 6):
+            v = math.pow(x, y)
+            v = math.factorial(v)
+            v //= (x + y)
+            v %= 982451653
+            table[(x, y)] = v
+
+    return table
+
+
+slowfun_table = build_lookup_table()
+
+
 def slowfun(x, y):
     # TODO: Modify to produce the same results, but much faster
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
-
-    return v
+    return slowfun_table[(x, y)]
 
 
+# Ranges include lower bound but NOT upper bound
 # Do not modify below this line!
 
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
-
-# Lookup table example:
-
-# import math
-# cache = {}
-
-# def build_lookup_table():
-#     for i in range(1, 1000):
-#         inv_squar_root(i)
-
-# def inv_squar_root(n):
-#     if n not in cache:
-#         cache[n] = 1 / math.sqrt
